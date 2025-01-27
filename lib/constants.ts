72f63b7fb67207640df78c1cfecb973d214dd2a8
mujsdev <mujtmurt@gmail.com>
2025-01-27 17:54:39 +0400

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,16 @@
 export const projects = [
   {
-    title: "3D Text and Minecraft Donuts",
-    description: "",
+    title: "🍩 3D Text and Minecraft Donuts",
+    description:
+      "A very simple 3D text with some matcap texture. Donuts with Minecraft texture. Why? I don't know.",
     url: "/3d-text-donuts",
+    badges: ["Rookie"],
+  },
+  {
+    title: "👻 Haunted House",
+    description:
+      "A pretty spooky haunted house. Had fun with building this and learned a lot how to piece all the fundamentals together. BOO! (did you get spooked?)",
+    url: "/haunted-house",
     badges: ["Rookie"],
   },
 ];
